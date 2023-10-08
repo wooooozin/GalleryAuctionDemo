5192b3ef1c32d1efe7d2de9eb284c7bf6b52ad1a
wooooozin <wooooozin@gmail.com>
2023-10-09 08:39:23 +0900

    feat: Artwork enum 생성 및 Artwork Entity 수정

--- src/main/java/com/woozi/auction/artwork/entity/Artwork.java
+++ src/main/java/com/woozi/auction/artwork/entity/Artwork.java
@@ -1,10 +1,13 @@
 package com.woozi.auction.artwork.entity;
 
+import com.woozi.auction.artwork.type.ArtworkStatus;
 import com.woozi.auction.category.entity.Category;
 import com.woozi.auction.user.entity.User;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -47,11 +50,12 @@ public class Artwork {
     @Column(name = "current_price", nullable = false)
     private Double currentPrice;
 
-    @Column(name = "status", nullable = false, length = 255)
-    private String status;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ArtworkStatus status = ArtworkStatus.ACTIVE;
 
     @Column(name = "is_deleted", nullable = false)
-    private Boolean isDeleted;
+    private Boolean isDeleted = false;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
