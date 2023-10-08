0bca51c3df8f3ac73df14c09ce08e63b3ab95272
wooooozin <wooooozin@gmail.com>
2023-10-08 17:29:43 +0900

    feat: Artwork entity 생성 및 Category, User entity 일부 생성

---
+++ src/main/java/com/woozi/auction/artwork/entity/Artwork.java
@@ -0,0 +1,64 @@
+package com.woozi.auction.artwork.entity;
+
+import com.woozi.auction.category.entity.Category;
+import com.woozi.auction.user.entity.User;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Artwork {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "artwork_id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Lob
+    @Column(name = "image", nullable = false)
+    private byte[] image;
+
+    @Column(name = "upload_date", nullable = false)
+    private LocalDateTime uploadDate;
+
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name = "starting_price", nullable = false)
+    private Double startingPrice;
+
+    @Column(name = "current_price", nullable = false)
+    private Double currentPrice;
+
+    @Column(name = "status", nullable = false, length = 255)
+    private String status;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
